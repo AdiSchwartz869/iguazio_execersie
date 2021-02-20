de9237ea20cfa9dbe799cb2f88cd3091ad780c8c
Ubuntu <ubuntu@ip-172-31-61-133.us-west-2.compute.internal>
2021-02-20 05:59:11 +0000

    weather.py change to work with python2.7

--- weather.py
+++ weather.py
@@ -2,9 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 search = "weather in tel aviv in celsius"
-URL = f"https://www.google.com/search?q={search}"
+URL = "https://www.google.com/search?q=" + search
 
 req = requests.get(URL)
 sav = BeautifulSoup(req.text, "html.parser")
 update = sav.find("div", class_ = "BNeawe").text
-print("The current weather in Tel-Aviv is:", update)
+print "The current weather in Tel-Aviv is: " , update
